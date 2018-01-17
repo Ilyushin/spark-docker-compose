c64c51ddeef214e85e9c57dd7f955c3fd98aa937
Evgeniy Ilyushin <john.ilyushin@gmail.com>
2018-01-17 14:55:56 +0300

    Added Kafka and Zookeeper for building

--- build-images.sh
+++ build-images.sh
@@ -6,10 +6,12 @@
 # Options argument scratch for deleting existing images and building the images from scratch
 
 if [ "$1" == scratch ]; then
-  docker rmi -f hadoop-spark hdfs-namenode hdfs-datanode spark-master spark-slave
+  docker rmi -f hadoop-spark hdfs-namenode hdfs-datanode spark-master spark-slave kafka zookeeper
 fi
 docker build -t hadoop-spark ./hadoop-spark/
 docker build -t hdfs-namenode ./hdfs-namenode/
 docker build -t hdfs-datanode ./hdfs-datanode/
 docker build -t spark-master ./spark-master/
 docker build -t spark-slave ./spark-slave/
+docker build -t kafka ./kafka/
+docker build -t zookeeper ./zookeeper/
